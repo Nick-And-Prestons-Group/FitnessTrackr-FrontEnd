8d02ef0f2e9913fa377809e3eab6b56f36abfe75
nickbloo <nickbloo2@gmail.com>
2022-11-16 21:11:13 -0600

    profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Link, useOutletContext, useParams, useNavigate, Navigate } from "react-router-dom";
+import RoutineDetails from "./RoutineDetails";
 
 const Profile = () => {
+    const currentToken = localStorage.getItem("token");
+    const navigate = useNavigate();
+    const [] = useOutletContext;
+
+    function logOutUser(event) {
+        event.preventDefault();
+        localStorage.removeItem("token");
+        navigate("/login");
+    };
+
     return (
         <div>
-            <p>You profile page</p>
+            {currentToken && currentToken.length ?
+            <div>
+                <form onSubmit={logOutUser}>
+                    <h2>Username: </h2>
+                    <h2>Name: </h2>
+                    <button type="submit">Log Out</button>
+                </form>
+                <br />
+                <RoutineDetails />
+            </div> : 
+            <div>
+                <p>Please log in or register for an account</p>
+                <Link to="/login">Click Here</Link>
+            </div> 
+            }
         </div>
     )
 };
