5abe8ca038dff0573384551c466e013e60b60b58
StroudEN <preston.jhowell@gmail.com>
2022-11-18 11:47:15 -0600

    Search engine added to activities, needs testing

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,21 +2,60 @@ import React, { useState, useEffect } from "react";
 import {Link, useOutletContext, useParams } from "react-router-dom";
 import NewActivity from "./NewActivity";
 
+
+
 const Activities = () => {
     const [activities] = useOutletContext();
     const currentToken = localStorage.getItem("token");
+    const [searchInput, setSearchInput] = useState("");
+
+    async function SearchActivities(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("http://placeholder.com/api/activities/search", {
+             
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    searchinput: searchInput
+                })
+            })
+            const searchData = await response.json()
+            console.log("these are the search results: ", searchData)
+        } catch (error) {
+            console.error
+        }
+    }
+
+    function updateSearchInput(event) {
+        setSearchInput(event.target.value)
+    }
 
     return (
         <div> { currentToken && currentToken.length ? 
             <NewActivity /> 
             : null }
-            { activities.map((eachActivity, idx) => {
-                return <div key={idx}>
-                    <h2>{eachActivity.name}</h2>
-                    <p><b>Description: </b>{eachActivity.description}</p>
-                </div>
-                })
+            <label for="searchbar">Search:</label>
+            <input type="text" id="searchbar" value={searchInput} onChange={updateSearchInput}></input>
+
+            { searchInput && searchInput.length ? 
+                    searchData.map((eachSearch, idx) => {
+                        return <div key={idx}>
+                            <h2> ${eachSearch.name} </h2>
+                            <p><b>Description:</b>{eachSearch.description}</p>
+                        </div>
+                    }) 
+  
+            :
+                    activities.map((eachActivity, idx) => {
+                        return <div key={idx}>
+                            <h2>{eachActivity.name}</h2>
+                            <p><b>Description: </b>{eachActivity.description}</p>
+                        </div>
+                     }) 
             }
+            
         </div>
     )
 };
